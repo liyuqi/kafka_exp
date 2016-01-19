fa48757df91e749006d911605a8bdcdd40011a43
liyuqi <am997liyuqi@gmail.com>
2016-01-19 13:56:50 +0800

    common kafka cmd

--- kafka_test_case/bt_kafka_cluster.sh
+++ kafka_test_case/bt_kafka_cluster.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
-ssh 172.17.24.22 "nohup kafka-server-start.sh kafka_exp/kafka-properties/server-z3-b5-p3.properties &"
-ssh 172.17.24.22 "nohup kafka-server-start.sh kafka_exp/kafka-properties/server-z3-b6-p3.properties &"
-nohup kafka-server-start.sh kafka_exp/kafka-properties/server-z3-b7-p3.properties &
-kafka-manager-1.3.0.4/bin/kafka-manager -Dconfig.file=kafka-manager-1.3.0.4/conf/application.conf -Dhttp.port=9001 &
+
+kafka-server-start.sh kafka_exp/kafka-properties/server-z3-b5-p3.properties &
+kafka-server-start.sh kafka_exp/kafka-properties/server-z3-b6-p3.properties &
+kafka-server-start.sh kafka_exp/kafka-properties/server-z3-b7-p3.properties &
+
+#JMX=10151 kafka-server-start.sh kafka_exp/kafka-properties/server-z3-b5-p3.properties &
+#JMX=10161 kafka-server-start.sh kafka_exp/kafka-properties/server-z3-b6-p3.properties &
+#JMX=10171 kafka-server-start.sh kafka_exp/kafka-properties/server-z3-b7-p3.properties &
+sudo ./kafka-manager -Dconfig.file=kafka-manager-1.3.0.4/conf/application-z3.conf -Dhttp.port=9001 &
+
+
+kafka-topics.sh --list --zookeeper 172.28.128.22:2181,172.28.128.23:2181,172.28.128.24:2181
+kafka-topics.sh --describe --zookeeper 172.28.128.22:2181,172.28.128.23:2181,172.28.128.24:2181 --topic cep_storm
+
+
+kafka-topics.sh --create --zookeeper 172.28.128.22:2181,172.28.128.23:2181,172.28.128.24:2181 -replication-factor 1 --partitions 3 --topic cep_storm
+kafka-topics.sh --alter --zookeeper 172.28.128.22:2181,172.28.128.23:2181,172.28.128.24:2181 --topic cep_storm --config replication-factor=1,partitions=3
+kafka-topics.sh --delete --zookeeper 172.28.128.22:2181,172.28.128.23:2181,172.28.128.24:2181 --topic cep_storm
+kafka-topics.sh --describe --zookeeper 172.28.128.22:2181,172.28.128.23:2181,172.28.128.24:2181 --topic cep_storm
+
+
+# confirm
