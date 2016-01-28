e9e0af85f824a268fab871640f3755c6a0ca2c1c
liyuqi <am997liyuqi@gmail.com>
2016-01-28 17:12:10 +0800

    update to --replication-factor

--- kafka_test_case/bt_kafka_cluster.sh
+++ kafka_test_case/bt_kafka_cluster.sh
@@ -23,8 +23,8 @@ kafka-topics.sh --describe --zookeeper 172.28.128.22:2181,172.28.128.23:2181,172
 kafka-topics.sh --describe --zookeeper 172.28.128.22:2181,172.28.128.23:2181,172.28.128.24:2181 --topic cep_storm
 
 
-kafka-topics.sh --create --zookeeper 172.28.128.22:2181,172.28.128.23:2181,172.28.128.24:2181 --partitions 1 -replication-factor 1 --topic kafkatest
-kafka-topics.sh --create --zookeeper 172.28.128.22:2181,172.28.128.23:2181,172.28.128.24:2181 --partitions 1 -replication-factor 1 --topic cep_storm
+kafka-topics.sh --create --zookeeper 172.28.128.22:2181,172.28.128.23:2181,172.28.128.24:2181 --partitions 1 --replication-factor 1 --topic kafkatest
+kafka-topics.sh --create --zookeeper 172.28.128.22:2181,172.28.128.23:2181,172.28.128.24:2181 --partitions 1 --replication-factor 1 --topic cep_storm
 #kafka-topics.sh --alter --zookeeper 172.28.128.22:2181,172.28.128.23:2181,172.28.128.24:2181 --topic cep_storm --config replication-factor=1,partitions=3
 #kafka-topics.sh --delete --zookeeper 172.28.128.22:2181,172.28.128.23:2181,172.28.128.24:2181 --topic cep_storm
 kafka-topics.sh --describe --zookeeper 172.28.128.22:2181,172.28.128.23:2181,172.28.128.24:2181 --topic cep_storm
